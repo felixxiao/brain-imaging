d836a3c137f620c74c7d2844c0a1befd5593bc4f
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-01-14 20:22:22 -0500

    Completed graph construction
    Used binary search since the reason for the previously slow code is
      that igraph passes by value when it modifies the graph. So every
      call of the for-loop previously had to construct a new graph
      object every time.
    It's much easier to add a LOT of edges at once. Hence, binary searching
      over the number of edges to add. Like before, we sort the edges
      by the distance-covariance (descending?? Check this.)
    
    Computing the distance-covariance (all 1.3 mil values) still takes
      a long time but maybe that's unavoidable. Need to look into
      parallel solutions.

--- energy_parcellation/source_graph.R
+++ energy_parcellation/source_graph.R
@@ -34,26 +34,37 @@ compute.edgeWeights = function(data, adj.list, func, verbose = FALSE)
 #construct a graph, edges weighed by energy dist. stop at
 #  specified number of connected components
 construct.graph <- function(data, adj.list, component.num = 20,
- verbose = TRUE){
+ verbose = TRUE, save = TRUE){
 
   edges = compute.edgeWeights(data, adj.list, dcor)
-  save(edges, file = paste0(PATH_DATA, "edges_", DATE, ".RData"))
+  if(save) save(edges, file = paste0(PATH_DATA, "edges_", 
+   DATE, ".RData"))
   if(verbose) cat("Edges done computing.")
 
   idx = order(edges$energy.vec, decreasing = TRUE)
   adj.mat = edges$adj.mat[idx,]
 
-  g = graph.empty(direct = F)
-  g = add.vertices(g, ncol(dat$dat))
- 
-  for (k in 1:nrow(adj.mat)){
-    g = add.edges(g, adj.mat[k,1:2])
+  g.base = graph.empty(ncol(dat$dat), direct = F)
 
-    current.comp = components(g)$no 
-    if (current.comp == component.num) break()
+  #let's do our binary search
+  upper = nrow(adj.mat)
+  lower = 1
+  compute.mid <- function(x,y){floor((x+y)/2)}
 
-    if(verbose && k %% floor(nrow(adj.mat)/100) == 0) 
-      print(current.comp)
+  while(TRUE){
+    mid = compute.mid(lower, upper)
+    g = add.edges(g.base, t(adj.mat[1:mid,]))
+
+    #add edges until you have component.num components. 
+    # too few comps -> add less edges
+    # too many comps -> add more edges
+    current.comp = components(g)$no
+    if(current.comp == component.num) break()
+    if(components(g)$no > component.num) lower = mid else upper = mid 
+
+    if(verbose) print(paste0("Current component number: ", current.comp))
   }
+  
+  g
 }
 
