b4a72c95c706c4a9b506e1fdef1104549f352444
felixxiao <felix.xiao1@gmail.com>
2016-01-24 00:28:59 -0500

    new R6Class implementing graphs with edge contraction. This will be used in a new agglomerative partitioning algorithm

---
+++ edge_contraction/source_contraction.R
@@ -0,0 +1,109 @@
+library(R6)
+library(assertthat)
+
+ContractibleGraph = R6Class('ContractibleGraph',
+  public = list(
+    # list, for each component, the vertices that constitute it
+    vertices = NA,
+    # list, for each component, list indexed by adjacent components
+    # of connecting edge weights as numeric vectors
+    edges = NA,
+    
+    # edges has duplicates
+    initialize = function(edges)
+    {
+      edge.mat = edges$edge.mat
+      energy.vec = edges$energy.vec
+      assert_that(length(energy.vec) == nrow(edge.mat))
+      
+      n = max(edge.mat)
+      self$vertices = as.list(1:n)
+      names(self$vertices) = as.character(1:n)
+      self$edges = lapply(1:n, function(x) list())
+      names(self$edges) = as.character(1:n)
+      
+      for (e in 1:nrow(edge.mat))
+      {
+        a = as.character(edge.mat[e,1])
+        b = as.character(edge.mat[e,2])
+        self$edges[[a]][[b]] = energy.vec[e]
+        if (! is.null(self$edges[[b]][[a]]))
+          assert_that(self$edges[[b]][[a]] == self$edges[[a]][[b]])
+      }
+    },
+    
+    get_edges = function(comp)
+    {
+      c = as.character(comp)
+      weights = sapply(self$edges[[c]], mean)
+      names(weights) = names(self$edges[[c]])
+      weights
+    },
+    
+    get_size = function(comp)
+    {
+      c = as.character(comp)
+      length(self$vertices[[c]])
+    },
+    
+    contract_components = function(comp1, comp2)
+    {
+      a = as.character(comp1)
+      b = as.character(comp2)
+      
+      if (is.null(self$edges[[a]][[b]]) | is.null(self$edges[[b]][[a]]))
+      {
+        assert_that(is.null(self$edges[[b]][[a]]) & is.null(self$edges[[a]][[b]]))
+        warning(comp1, ' and ', comp2, ' are not connected')
+        return()
+      }
+      
+      assert_that(length(intersect(self$vertices[[a]], self$vertices[[b]])) == 0)
+      self$vertices[[a]] = c(self$vertices[[a]], self$vertices[[b]])
+      
+      for (b.adj in names(self$edges[[b]]))
+      {
+        self$edges[[a]][[b.adj]] = c(self$edges[[a]][[b.adj]],
+                                     self$edges[[b]][[b.adj]])
+        self$edges[[b.adj]][[a]] = self$edges[[a]][[b.adj]]
+        self$edges[[b.adj]][[b]] = NULL
+      }
+      self$edges[[a]][[a]] = NULL
+
+      self$vertices[[b]] = NULL
+      self$edges[[b]] = NULL
+      
+      a
+    }
+  )
+)
+
+edges = list()
+edges$edge.mat = t(matrix(c(1, 2,
+                            1, 3,
+                            2, 1,
+                            2, 4,
+                            3, 1,
+                            3, 4,
+                            4, 2,
+                            4, 3),
+                          nrow = 2))
+edges$energy.vec = c(0.7, 0.8, 0.7, 0.5, 0.8, 0.9, 0.5, 0.9)
+#        0.7
+#    (1) --- (2)
+# 0.8 |       | 0.5
+#    (3) --- (4)
+#        0.9
+
+cg = ContractibleGraph$new(edges)
+cg$get_edges(1)              # 0.7 0.8
+cg$get_size(4)               # 1
+cg$contract_components(1, 4) # warning, not connected
+cg$contract_components(1, 2) # new component 1
+cg$get_size(1)               # 2
+cg$get_edges(1)              # 0.8 0.5
+cg$contract_components(3, 4) # new component 3
+cg$get_edges(3)              # (0.8 + 0.5) / 2 = 0.65
+
+cg$contract_components(1, 3)
+cg$get_size(1)               # 4
