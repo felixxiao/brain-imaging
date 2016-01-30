4cdffadf6b91847605801c36b450ad13a98cff07
felixxiao <felix.xiao1@gmail.com>
2016-01-29 21:14:01 -0500

    edge.mat creation defaults no duplicates; added function for removing zero-voxels from data and edge matrix

--- energy_parcellation/header_energy.R
+++ energy_parcellation/header_energy.R
@@ -2,6 +2,7 @@ suppressPackageStartupMessages(library(igraph))
 suppressPackageStartupMessages(library(energy))
 suppressPackageStartupMessages(library(testthat))
 suppressPackageStartupMessages(library(assertthat))
+suppressPackageStartupMessages(library(plyr))
 
 PATH_DATA = 'C:/Users/Felix/Dropbox/Felix_Kevin_Han-seniorthesis2015-16/data/'
 PATH_SAVE = PATH_DATA
