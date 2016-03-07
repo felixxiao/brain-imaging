23d55a65f70ad4462f64750f1d4b8a5eec494bfe
felixxiao <felix.xiao1@gmail.com>
2016-03-07 18:04:55 -0500

    faster way to get all pairs of vertices a certain distance apart

--- graph_structure/adjacency.R
+++ graph_structure/adjacency.R
@@ -1,48 +1,25 @@
-source('header_global.R')
 source('preprocess/header_preprocess.R')
-library(plyr)
+source('graph_structure/header_structure.R')
+source('energy_parcellation/header_energy.R')
 
-load(paste0(PATH_DATA, 'ABIDE_50002_matrix_2015-12-07.RData'))
 load(paste0(PATH_DATA, 'template_2015-12-07.RData'))
 MNI = load.nifti(paste0(PATH_DATA, 'MNI152_T1_2mm_brain_symmetric.nii.gz'))
 
 dimen = dim(MNI)
 mask = template$mask
+rm(MNI, template)
 
 loc = sapply(mask, .convert.2Dto3Dloc, dimen = dimen, simplify = 'matrix')
 
-link.cross1 = function(ijk, dimen)
-{
-  i = ijk[1]; j = ijk[2]; k = ijk[3]
-  
-  a = matrix(c(i  , j  , k  ,
-               i  , j  , k  ,
-               i  , j  , k  ,
-               i+1, j  , k  ,
-               i  , j+1, k  ,
-               i  , j  , k+1), nrow = 3)
-  apply(a, 2, .convert.3Dto2Dloc, dimen = dimen)
-}
-link.cross2 = function(ijk, dimen)
-{
-  i = ijk[1]; j = ijk[2]; k = ijk[3]
+time = Sys.time()
+edge.mat2 = edgemat(2, loc, dimen, mask)
+Sys.time() - time
 
-  b = matrix(c(i  , j+1, k+1,
-               i+1, j  , k+1,
-               i+1, j+1, k  ,
-               i  , j+1, k  ,
-               i  , j  , k+1,
-               i+1, j  , k  ), nrow = 3)
-  apply(b, 2, .convert.3Dto2Dloc, dimen = dimen)
-}
+load(paste0(PATH_DATA, 'ABIDE_50002_matrix_2015-12-07.RData'))
+weights = compute.edgeWeights(dat$dat, NULL, dcor, edge.mat2, verbose = T, save = F)
+weights = weights$energy.vec
 
-link1 = as.vector(apply(loc, 2, link.cross1, dimen = dimen))
-link2 = as.vector(apply(loc, 2, link.cross2, dimen = dimen))
-valid.link = link1 %in% mask & link2 %in% mask
-link = cbind(link1, link2)[valid.link,]
-rm(link1, link2, valid.link)
+mean(weights)
+hist(weights)
 
-link = plyr::mapvalues(link, mask, 1:length(mask))
-
-source('energy_parcellation/header_energy.R')
-weights = compute.edgeWeights(dat$dat, NULL, dcor, link, verbose = T, save = F)
+save(link, weights, file = paste0('graph_structure/2-3.RData'))
---
+++ graph_structure/header_structure.R
@@ -0,0 +1,36 @@
+source('header_global.R')
+library(plyr)
+library(gtools)
+
+neighbor.indices = function(dist_sqrd)
+{
+  n = ceiling(sqrt(dist_sqrd)) + 1
+  A = array(0, dim = c(n, n, n))
+  for (i in 1:n)
+    for (j in 1:n)
+      for (k in 1:n)
+        A[i,j,k] = (i-1)^2 + (j-1)^2 + (k-1)^2
+  idx.nonneg = arrayInd(which(A == dist_sqrd), dim(A)) - 1
+  signs = gtools::permutations(2, 3, c(1,-1), repeats.allowed = T)
+  idx = mapply(kronecker, split(signs, col(signs)),
+               split(idx.nonneg, col(idx.nonneg)))
+  unique(idx, MARGIN = 1)
+}
+
+edgemat = function(dist_sqrd, loc, dimen, mask)
+{
+  idx = neighbor.indices(dist_sqrd)
+  edge.mat = lapply(1:ncol(loc), function(j)
+    {
+      neighbors.1D = apply(sweep(idx, 2, loc[,j], '+'), 1,
+                           .convert.3Dto2Dloc, dimen = dimen)
+      cbind(mask[j], neighbors.1D, deparse.level = 0)
+    })
+  edge.mat = do.call(rbind, edge.mat)
+  edge.mat = t(apply(edge.mat, 1, sort))
+  edge.mat = unique(edge.mat, MARGIN = 1)
+  edge.mat = edge.mat[edge.mat[,1] %in% mask & edge.mat[,2] %in% mask,]
+
+  plyr::mapvalues(edge.mat, mask, 1:length(mask), warn_missing = F)
+}
+
